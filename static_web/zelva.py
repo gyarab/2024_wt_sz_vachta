5f6830cc95d66e8cba86f791781876198c51f8fd
Oliver Vachta <oliver.vachta.s@gyarab.cz>
2025-02-12 12:35:10 +0100

    Why so serious ;)

---
+++ static_web/zelva.py
@@ -0,0 +1,16 @@
+from turtle import forward, exitonclick, left, right
+
+left(45)
+forward(54.447)
+right(90)
+forward(54.447)
+right(135)
+forward(77)
+left(90)
+forward(77)
+left(90)
+forward(77)
+left(90)
+forward(77)
+
+exitonclick()
